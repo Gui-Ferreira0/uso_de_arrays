dd35b3c60b41e3cdab2c67ed3900253c635a11c5
Gui-Ferreira0 <smartgui2010@gmail.com>
2026-05-20 13:12:25 -0300

    uso_de_arrays

---
+++ ex1_alunos/script.js
@@ -0,0 +1,6 @@
+const alunos = [{nome: "Rafael", disciplinaPreferida: "lanche",idade: 16, },
+                {nome: "Saitama", disciplinaPreferida: "filosofia",idade: 25 },
+                {nome: "Gojo", disciplinaPreferida: "matemática",idade: 30 },
+                {nome: "Goku", disciplinaPreferida: "educação",idade: 43 },
+                {nome: "Leonidas", disciplinaPreferida: "história",idade: 43}];
+alert("nome do aluno: " + alunos[0].nome + " e matéria favorita: " + alunos[0].disciplinaPreferida);
